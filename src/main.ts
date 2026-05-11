19958ad936e37fe75d80b0b498fe74a9874f49d9
nguyen5562 <nknguyen112345@gmail.com>
2026-05-11 20:49:34 +0700

    update https

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://map.nguyenkhoi.io.vn'],
+    origin: ['https://map.nguyenkhoi.io.vn'],
     credentials: true,
   });
   app.setGlobalPrefix('api');
