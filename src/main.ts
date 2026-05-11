bf0ea5d343ebdc00de7404d05e2c6e568ae08ab6
nguyen5562 <nknguyen112345@gmail.com>
2026-05-11 20:48:10 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://map.nguyenkhoi.io.vn'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
